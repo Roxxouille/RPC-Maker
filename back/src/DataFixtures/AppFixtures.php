9052615d671f302d6068bdc3081997c7d0919996
Etudiant O'clock <raphael.huiban.le.corff@gmail.com>
2020-07-24 13:21:56 +0200

    [build] (symfo) add faker

--- back/src/DataFixtures/AppFixtures.php
+++ back/src/DataFixtures/AppFixtures.php
@@ -2,7 +2,14 @@
 
 namespace App\DataFixtures;
 
+use Faker;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
 class AppFixtures extends Fixture
 {
-    
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create();
+    }
 }
