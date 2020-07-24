6a5549eccae2fdc3cc3dc77631f5cf7d59d3a774
Adi9378 <paulabnsr@gmail.com>
2020-07-24 22:07:57 +0200

    [build]Accueil0.6

--- front/src/components/Cards/index.js
+++ front/src/components/Cards/index.js
@@ -5,26 +5,26 @@ import "./styles.scss";
 const Cards = () => {
     return (
         <div className="show-team">
-            <Card className="show-team__one" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card className="show-team__one" style={{ width: '16rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Alexis</Card.Title>
                 </Card.Body>
             </Card>
-            <Card className="show-team__one" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card className="show-team__one" style={{ width: '16rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Raphaël</Card.Title>
                 </Card.Body>
             </Card>
-            <Card className="show-team__one" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card className="show-team__one" style={{ width: '16rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Sébastien</Card.Title>
                 </Card.Body>
             </Card>
-            <Card className="show-team__one" style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+            <Card className="show-team__one" style={{ width: '16rem' }}>
+                <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Paul</Card.Title>
                 </Card.Body>
