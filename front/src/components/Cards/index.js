7e96e0acd10dd78719849250f1f0052e07320f48
Adi9378 <paulabnsr@gmail.com>
2020-07-25 16:08:12 +0200

    [build] footer edited

--- front/src/components/Cards/index.js
+++ front/src/components/Cards/index.js
@@ -9,24 +9,28 @@ const Cards = () => {
                 <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Alexis</Card.Title>
+                    <p>Developpeur Symfony</p>
                 </Card.Body>
             </Card>
             <Card className="show-team__one" style={{ width: '16rem' }}>
                 <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Raphaël</Card.Title>
+                    <p>Developpeur Symfony</p>
                 </Card.Body>
             </Card>
             <Card className="show-team__one" style={{ width: '16rem' }}>
                 <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Sébastien</Card.Title>
+                    <p>développeur React</p>
                 </Card.Body>
             </Card>
             <Card className="show-team__one" style={{ width: '16rem' }}>
                 <Card.Img variant="top" src="https://picsum.photos/160/120" />
                 <Card.Body>
                     <Card.Title>Paul</Card.Title>
+                    <p>développeur React</p>
                 </Card.Body>
             </Card>
         </div>
