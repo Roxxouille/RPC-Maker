6cf1d7523c7351ff1c42db759eec190a0118ae3a
Etudiant O'clock <raphael.huiban.le.corff@gmail.com>
2020-07-27 20:20:34 +0200

    [feat] (User) When adding a new user, auto add apiToken

--- back/src/EventListener/UserListener.php
+++ back/src/EventListener/UserListener.php
@@ -2,10 +2,12 @@
 
 namespace App\EventListener;
 
+use App\Entity\User;
+
 class UserListener
 {
-    public function prePersist()
+    public function prePersist(User $user)
     {
-        
+        $user->setApiToken(md5(uniqid(rand(), true)));
     }
 }
