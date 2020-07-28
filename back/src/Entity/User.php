0216aa9edd5468b0efac815dafc56729fdac7099
Y0up <alexis.dangelo.marquine@gmail.com>
2020-07-28 12:05:41 +0200

    [feat](User.php)added a json response for unique property

--- back/src/Entity/User.php
+++ back/src/Entity/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use App\Entity\Avatar;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\Collection;
@@ -9,10 +10,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\HasLifecycleCallbacks()
+ * @UniqueEntity("email")
+ * @UniqueEntity("username")
  */
 class User implements UserInterface
 {
