6cdfc588411e4f3835ebe03fd3bec0ec88931f9b
Adi9378 <paulabnsr@gmail.com>
2020-07-28 13:22:14 +0200

    [build] last commit

--- front/src/middleware/user.js
+++ front/src/middleware/user.js
@@ -7,9 +7,9 @@ export default (store) => (next) => (action) => {
   switch (action.type) {
     case LOGIN: {
       const state = store.getState();
-      const email = state.email;
-      const password = state.password;
-      axios.post('http://localhost:3000/api/login', { email: email, password: password })
+      const {email, password} = state;
+      console.log(email, password);
+      axios.post('http://localhost:3000/api/login', {data: { username: 'shemar51@gmail.com', password: "user" }}, { headers: {'X-AUTH-TOKEN': '', 'Content-Type': 'application/json' }})
         .then((response) => {
           console.log(response);
         })
