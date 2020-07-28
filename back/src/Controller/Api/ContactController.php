70fdcbdd511a6a6bd61bb8233f7806bd2d631a6a
Etudiant O'clock <raphael.huiban.le.corff@gmail.com>
2020-07-28 10:33:32 +0200

    [build](mailer) install of google-mailer

---
+++ back/src/Controller/Api/ContactController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    public function sendEmail(Request $request)
+    {
+        # code...
+    }
+}
