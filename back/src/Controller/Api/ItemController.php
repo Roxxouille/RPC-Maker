e888cdbdf93c1f28aece660317aa7dbbf1f756c8
Y0up <alexis.dangelo.marquine@gmail.com>
2020-07-28 10:52:57 +0200

    [feat](itemController.php)send a json 404 status for read

--- back/src/Controller/Api/ItemController.php
+++ back/src/Controller/Api/ItemController.php
@@ -4,8 +4,9 @@ namespace App\Controller\Api;
 
 use App\Entity\Item;
 use App\Repository\ItemRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ItemController extends AbstractController
 {
@@ -23,8 +24,12 @@ class ItemController extends AbstractController
      /**
      * @Route("api/item/{id<\d+>}", name="item", methods="GET")
      */
-    public function getOne($id, ItemRepository $itemRepository, Item $item)
+    public function getOne($id, ItemRepository $itemRepository, Item $item = null)
     {
+        //send a 404 error if the item does not exist
+        if ($item === null) {
+            return $this->json(['error' => 'item non trouve'], Response::HTTP_NOT_FOUND);
+        }
 
         $data = $itemRepository->findOneByCategory($item);
         
