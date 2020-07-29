c7384553bb280d1bbe2cbb53ba7f7fa2c0276ca9
Y0up <alexis.dangelo.marquine@gmail.com>
2020-07-29 12:30:12 +0200

    [test](ApplicationAvailabilityFunctionalTest.php)added doctrine-test-bundle + enable it in phpunit.xml.dist + created an AvatarRepositoryTest file

---
+++ back/tests/Repository/AvatarRepositoryTest.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Tests\Repository;
+
+use App\Entity\Avatar;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AvatarRepositoryTest extends KernelTestCase
+{
+
+}
