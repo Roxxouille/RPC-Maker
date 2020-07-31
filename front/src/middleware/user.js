6cf1bdab9b9c3017068b4d363fe79015d29bf9b0
alexis.dangelo <alexis.angelo.marquine@gmail.com>
2020-07-31 15:55:18 +0200

    change header

--- front/src/middleware/user.js
+++ front/src/middleware/user.js
@@ -13,11 +13,7 @@ export default (store) => (next) => (action) => {
     case LOGIN: {
       const state = store.getState();
       const { email, password } = state;
-<<<<<<< .merge_file_a21064
       axios.post('http://localhost:3000/login', { username: email, password, login: true }, { headers: { 'Content-Type': 'application/json' } })
-=======
-      axios.post('http://54.173.92.69/api/login', { username: email, password, login: true }, { headers: { 'Content-Type': 'application/json' } })
->>>>>>> .merge_file_a18632
         .then((response) => {
           console.log(response);
           localStorage.setItem('slug', response.data.slug);
@@ -35,11 +31,7 @@ export default (store) => (next) => (action) => {
       const token = localStorage.getItem('token');
       //const token = 'd6081bdf250ec5c06a1bc2dd28bba8b0';
       const slug = localStorage.getItem('slug');
-<<<<<<< .merge_file_a21064
       axios.get(`http://localhost:3000/user/${slug}`, { headers: { 'X-AUTH-TOKEN': token, 'Content-Type': 'application/json' } })
-=======
-      axios.get(`http://54.173.92.69/api/user/${slug}`, { headers: { 'X-AUTH-TOKEN': token, 'Content-Type': 'application/json' } })
->>>>>>> .merge_file_a18632
         .then((response) => {
           console.log(response);
           store.dispatch(setUser(response.data.username));
