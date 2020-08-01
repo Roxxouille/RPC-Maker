4e0ea3d07c796d199956fded44e98dbc4cc0d3df
alexis.dangelo <alexis.angelo.marquine@gmail.com>
2020-08-01 17:28:06 +0200

    [feat](profil)reformed the profil section

--- front/src/components/User/index.js
+++ front/src/components/User/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import {
   Switch,
   Route,
@@ -8,6 +8,13 @@ import {
 import PropTypes from 'prop-types';
 import './styles.scss';
 import { activateLoad } from '../../actions/user';
+import {
+  Container,
+  Jumbotron,
+  Image,
+  Tabs,
+  Tab,
+} from 'react-bootstrap';
 
 const User = ({ isLogged, isLoading }) => {
   let load = isLoading;
@@ -18,7 +25,20 @@ const User = ({ isLogged, isLoading }) => {
   console.log(isLoading);
 
   return (
-    <div className="container">
+
+    <Container fluid>
+
+
+
+      <Jumbotron fluid className="jumbotron">
+        <Container>
+          <h1>Bienvenu "username"</h1>
+          <p>
+            Dans cette section vous pouvez gerer tout ce qui vous concerne et aussi contacter nos brillant monteur!
+    </p>
+        </Container>
+      </Jumbotron>
+
 
       {isLoading && localStorage.getItem('token') && (
         <p>ça charge gros</p>
@@ -30,13 +50,32 @@ const User = ({ isLogged, isLoading }) => {
 
       {isLogged === true && isLoading === false && (
         <div className="user">
+
+
           <div className="user__nav">
-            <span>AVATAR</span>
-            <Link to="/user/pc">Mon PC</Link>
-            <Link to="/user/infos">mes informations</Link>
-            <Link to="/user/message">Messagerie</Link>
+            <Image src="https://picsum.photos/240" roundedCircle fluid />
+            <Link to="/user/pc">"username"</Link>
+            <a>"Level"</a>
+            <a>"firstname" "lastname"</a>
+            <a>"adress"</a>
+            <a href="#">Editer mon profil</a>
+            <a href="#">Changer de mot de passe</a><a href="#">Se deconnecter</a>
+            {/* <Link to="/user/infos">mes informations</Link>
+            <Link to="/user/message">Messagerie</Link> */}
           </div>
           <div className="user__body">
+
+            <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+              <Tab eventKey="home" title="PC">
+
+              </Tab>
+              <Tab eventKey="profile" title="commandes">
+
+              </Tab>
+              <Tab eventKey="contact" title="messagerie">
+
+              </Tab>
+            </Tabs>
             <Switch>
               <Route exact path="/user">
                 Bienvenue dans votre espace membre
@@ -54,7 +93,7 @@ const User = ({ isLogged, isLoading }) => {
           </div>
         </div>
       )}
-    </div>
+    </Container>
   );
 };
 
