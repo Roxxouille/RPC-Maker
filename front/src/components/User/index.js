48ad20ab7d057d965567c83704c2373088c444bc
alexis.dangelo <alexis.angelo.marquine@gmail.com>
2020-08-04 11:29:22 +0200

    [refacto](user)created js file to separate the code for more readability

--- front/src/components/User/index.js
+++ front/src/components/User/index.js
@@ -6,6 +6,10 @@ import {
   Redirect,
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Command from './Command.js';
+import Message from './Message.js';
+import Info from './Info.js';
+import Pc from './Pc.js';
 import './styles.scss';
 import { activateLoad } from '../../actions/user';
 import {
@@ -13,16 +17,6 @@ import {
   Jumbotron,
   Image,
   Button,
-  Accordion,
-  Card,
-  Table,
-  Col,
-  Row,
-  Form,
-  Group,
-  InputGroup,
-  FormControl,
-
 } from 'react-bootstrap';
 import { FaDownload } from 'react-icons/fa';
 
@@ -75,57 +69,7 @@ const User = ({ isLogged, isLoading }) => {
 
             <Switch>
               <Route path="/user/edit-info">
-                <Container>
-                  <div>
-                    <h1>Mes Informations :</h1>
-                  </div>
-                  <Form>
-                    <Form.Row>
-                      <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                      </Form.Group>
-                    </Form.Row>
-
-                    <Form.Row>
-                      <Form.Group as={Col} controlId="formGridFirstname">
-                        <Form.Label>Prenom</Form.Label>
-                        <Form.Control placeholder="firstname" />
-                      </Form.Group>
-
-                      <Form.Group as={Col} controlId="formGridLastname">
-                        <Form.Label>Nom</Form.Label>
-                        <Form.Control placeholder="lastname" />
-                      </Form.Group>
-                    </Form.Row>
-
-                    <Form.Group controlId="formGridAddress1">
-                      <Form.Label>Address</Form.Label>
-                      <Form.Control placeholder="adress" />
-                    </Form.Group>
-
-                    <Form.Group controlId="formGridAddress2">
-                      <Form.Label>Address 2</Form.Label>
-                      <Form.Control placeholder="adress precision" />
-                    </Form.Group>
-
-                    <Form.Row>
-                      <Form.Group as={Col} controlId="formGridCity">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control placeholder="city" />
-                      </Form.Group>
-
-                      <Form.Group as={Col} controlId="formGridZip">
-                        <Form.Label>Zip</Form.Label>
-                        <Form.Control placeholder="zipcode" />
-                      </Form.Group>
-                    </Form.Row>
-
-                    <Button variant="primary" type="submit">
-                      Submit
-                  </Button>
-                  </Form>
-                </Container>
+                <Info />
 
               </Route>
 
@@ -136,157 +80,16 @@ const User = ({ isLogged, isLoading }) => {
 
 
               <Route path="/user/pc">
-                <Accordion defaultActiveKey="0">
-                  <Card id="center">
-                    <Accordion.Toggle as={Card.Header} eventKey="0" id="center">
-                      "nom du pc"
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="0">
-                      <Card.Body>
-                        <Container >
-                          <Row >
-                            <Col sm={2} > <Image src="https://picsum.photos/240" rounded fluid /></Col>
-                            <Col sm={10}>
-                              <Container>
-                                <Row>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                  <Col sm={3}>
-                                    <h6>Carte graphique</h6>
-                                    <p>RTX 2080</p>
-                                  </Col>
-                                </Row>
-                              </Container>
-                            </Col>
-                          </Row>
-                        </Container>
-                      </Card.Body>
-                    </Accordion.Collapse>
-                  </Card>
-                </Accordion>
+                <Pc />
               </Route>
+
               <Route path="/user/commands">
-                <div>
-                  <h1>Mes Commandes :</h1>
-                </div>
-                <Accordion defaultActiveKey="0">
-                  <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="0" id="center">
-                      "nom de la commande"
-                    </Accordion.Toggle>
-                    <Accordion.Collapse eventKey="0">
-                      <Card.Body>
-                        <Table striped bordered hover size="sm">
-                          <thead>
-                            <tr>
-                              <th id="center">Facture</th>
-                              <th>Composant</th>
-                              <th>Model</th>
-                              <th>Prix</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td id="center"><FaDownload /></td>
-                              <td>Carte graphique</td>
-                              <td>RTX 2080 Ti</td>
-                              <td>350 euros</td>
-                            </tr>
-                            <tr>
-                              <td id="center"><FaDownload /></td>
-                              <td>Processeur</td>
-                              <td>intel i7 9700</td>
-                              <td>450 euros</td>
-                            </tr>
-                            <tr>
-                              <td colSpan="3">TOTAL</td>
-                              <td>800 euros</td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                      </Card.Body>
-                    </Accordion.Collapse>
-                  </Card>
-                </Accordion>
+                <Command />
               </Route>
 
 
               <Route path="/user/message">
-
-                <Container classname="user__body__messages">
-                  <div id="center">
-                    <h1>Vous etes en conversation avec "nom-monteur"</h1>
-                  </div>
-                  <Row>
-                    <Col sm={2}>
-                      <Image src="https://picsum.photos/240" rounded fluid id="chat-avatar" />
-                    </Col>
-                    <Col sm={10}>
-                      <p>
-                        "nom-monteur":
-                      <br />
-                      Bonjour en quoi puis je vous aider?
-                      <br />
-                      le 12/04/2020 a 17h55
-                     </p>
-                    </Col>
-                  </Row>
-                  <Row id="just-center" >
-                    <Col sm={{ span: 8, offset: 2 }}>
-                      <p>
-                        "nom-monteur":
-                      <br />
-                      Bonjour en quoi puis je vous aider?
-                      <br />
-                      le 12/04/2020 a 17h55
-                     </p>
-                    </Col>
-                    <Col sm={2}>
-                      <Image src="https://picsum.photos/240" rounded fluid id="chat-avatar" />
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col sm={10}>
-                      <InputGroup>
-
-                        <FormControl as="textarea" aria-label="With textarea" />
-                      </InputGroup>
-                    </Col>
-                    <Col sm={2}>
-                      <Button variant="secondary">Envoyer</Button>{' '}
-                    </Col>
-                  </Row>
-                </Container>
+                <Message />
               </Route>
 
 
