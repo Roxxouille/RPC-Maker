e90352edcf97a6f61f2c87932275dc55550d3f55
Adi9378 <paulabnsr@gmail.com>
2020-08-11 18:52:08 +0200

    add progression

--- front/src/components/Account/User/Progress.js
+++ front/src/components/Account/User/Progress.js
@@ -5,21 +5,21 @@ const Progress = () => {
     <div id="timeline-content">
       <h1>Ma progression</h1>
 
-      <ul class="timeline">
-        <li class="event" data-date="Level 1">
+      <ul className="timeline">
+        <li className="event" data-date="Level 1">
           <h3>T'es vraiment un bg</h3>
           <p>Bah oui t'as réussi a remplir un formulaire gros</p>
         </li>
-        <li class="event" data-date="level 2">
+        <li className="event" data-date="level 2">
           <h3>Doublement BG</h3>
           <p>t'as dû faire un truc en +</p>    
         </li>
-        <li class="event" id="date" data-date="level 3">
+        <li className="event" id="date" data-date="level 3">
           <h3>J'ai plus d'inspiration</h3>
           <p>rien à dire</p>    
         </li>
         
-        <li class="event" data-date="level 4">
+        <li className="event" data-date="level 4">
           
           <h3>Squareflair Today</h3>
           
