db3b09169a2b61965239faf7e93565d0de82045b
alexis.dangelo <alexis.angelo.marquine@gmail.com>
2020-08-11 12:00:27 +0200

    [redesign](backoffice)redesign of the tchat page

--- front/src/components/Account/BackOffice/Conversations.js
+++ front/src/components/Account/BackOffice/Conversations.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ListGroup, Form, Row } from 'react-bootstrap';
+import { ListGroup, Form, Row, Image } from 'react-bootstrap';
 import Messages from 'src/components/Account/Messages';
+import { FaUsers, FaUber, FaUser, FaTv } from 'react-icons/fa';
 
 const Conversations = ({ clients, getMessagesBack, messages, username, newMessage, changeMessageBack, sendMessageBack, activeConv }) => {
   console.log('Clients component conv', clients);
@@ -21,27 +22,63 @@ const Conversations = ({ clients, getMessagesBack, messages, username, newMessag
   const dataClients = clients.map((client) => {
     const activate = client.slug === activeConv;
     return (
-      <ListGroup.Item active={activate} onClick={handleClick} title={client.id} id={client.slug} key={client.slug}>
-        {client.username}
+      <ListGroup.Item className="backoffice__body__tchat__clients__list__client" active={activate} onClick={handleClick} title={client.id} id={client.slug} key={client.slug}>
+
+        <p>{client.username}</p>
+
       </ListGroup.Item>
     );
   });
 
   return (
-    <div className="tchat">
-      <ListGroup className="tchat__clients">
-        {dataClients}
-      </ListGroup>
-      <div className="tchat__messages">
-        <Messages messages={messages} username={username} />
-        <Form onSubmit={handleSubmit}>
-          <Row>
-            <Form.Control onChange={handleChange} name='newMessageBack' type='text' value={newMessage} placeholder='Tapez votre message' />
-          </Row>
-        </Form>
-      </div>
 
-    </div>
+    <div className="backoffice__body__tchat">
+      <div className="backoffice__body__tchat__clients" >
+        <div className="backoffice__body__tchat__clients__header">
+          <h3>Mes clients</h3>
+          <h3><FaUsers /></h3>
+        </div>
+        <div className="backoffice__body__tchat__clients__list">
+          {dataClients}
+        </div>
+      </div>
+      <div className="backoffice__body__tchat__messages" >
+        <div className="backoffice__body__tchat__messages__header">
+          <div className="backoffice__body__tchat__messages__header__info">
+            <h2>Jean Dupont</h2>
+            <p>username</p>
+          </div>
+          <div className="backoffice__body__tchat__messages__header__icons">
+            <h3 className="backoffice__body__tchat__messages__header__icons__icon"><FaUser /></h3>
+            <h3 className="backoffice__body__tchat__messages__header__icons__icon"><FaTv /></h3>
+
+          </div>
+
+
+        </div>
+        <div className="backoffice__body__tchat__messages__body">
+          <div className="backoffice__body__tchat__messages__body__content">
+            <Messages messages={messages} username={username} />
+            <Form onSubmit={handleSubmit}>
+              <Form.Control onChange={handleChange} name='newMessageBack' type='text' value={newMessage} placeholder='Tapez votre message' />
+            </Form>
+          </div>
+          <div className="backoffice__body__tchat__messages__body__profile">
+            <Image src="https://picsum.photos/240" rounded />
+            <p>Nom Prenom</p>
+            <p>username</p>
+            <hr />
+            <p>email</p>
+            <hr />
+            <p>adress</p>
+            <hr />
+            <p>dataform</p>
+
+          </div>
+        </div>
+      </div>
+    </div >
+
   );
 };
 
--- front/src/components/Account/Messages.js
+++ front/src/components/Account/Messages.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import {Row, Col, Image} from 'react-bootstrap';
+import { Row, Col, Image } from 'react-bootstrap';
 
 const Messages = ({ messages, username }) => {
   console.log(messages);
@@ -8,13 +8,9 @@ const Messages = ({ messages, username }) => {
     const senderName = message.fromUser.username !== username ? <>{message.fromUser.firstname} {message.fromUser.className} <br /></> : '';
     return (
       <Row key={message.id} className={message.fromUser.username === username ? 'sent message' : 'received message'}>
-        <Col sm={1}>
-            <Image src="https://picsum.photos/240" rounded fluid id="chat-avatar" />
-        </Col>
-        <Col sm={5}>
+        <Col sm={6} style={{ margin: '20px' }}>
           <p>
-            {senderName}
-              {message.content}
+            {message.content}
             <br />
             {message.created_at}
           </p>
